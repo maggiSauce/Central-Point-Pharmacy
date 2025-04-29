986c40fc54248b09616256b19469386210cef81c
Ben B <bbui@ualberta.ca>
2025-04-29 16:43:42 -0600

    can parse

--- StudentForms/src/CSVtoStudentPDF.py
+++ StudentForms/src/CSVtoStudentPDF.py
@@ -1,1 +1,25 @@
-print("hello world")
+import csv
+
+def openFile(filepath:str) -> list:
+    '''
+    opens and formats a file
+    returns a list of each line in the file
+    '''
+    
+    with open(filepath, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+    
+        # Get the first row
+        first_row = next(reader)
+
+        # Filter out empty values (None, empty strings, or whitespace-only strings)
+        filledFieldsDict = {
+            key: value for key, value in first_row.items() if value and value.strip()
+        }
+    return filledFieldsDict
+
+def main():
+    test = openFile(r"C:\Users\small\Central-Point-Pharmacy\StudentForms\Patient listing report.csv")
+    print(test)
+
+main()
